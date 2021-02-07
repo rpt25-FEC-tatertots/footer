637392f75254870ae1a8078d26dbca52debf538c
Mason Pott <masoncpott@gmail.com>
2021-02-07 16:36:51 -0600

    connected to the server and the DOM

---
+++ client/src/FooterComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+function FooterComponent() {
+  return (
+    <div>Hello  World</div>
+  )
+}
+
+ReactDOM.render(<FooterComponent />, document.getElementById('footer'));
